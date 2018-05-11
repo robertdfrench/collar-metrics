0a75042904abc12815c906aaa148aa626cf41790
Robert D. French <robert@robertdfrench.me>
2018-05-11 03:03:57 -0400

    Distinguish barks by collar ids

--- collar_metrics/__init__.py
+++ collar_metrics/__init__.py
@@ -3,14 +3,14 @@ import flask
 
 def bootstrap(app, barks):
 
-    @app.route("/collar/1/barks", methods=['GET'])
-    def list_barks():
-        return flask.jsonify(data=barks.by_collar('1'))
+    @app.route("/collar/<collar_id>/barks", methods=['GET'])
+    def list_barks(collar_id):
+        return flask.jsonify(data=barks.by_collar(collar_id))
 
 
-    @app.route("/collar/1/barks/new", methods=['POST'])
-    def add_barks():
+    @app.route("/collar/<collar_id>/barks/new", methods=['POST'])
+    def add_barks(collar_id):
         for bark in flask.request.json['data']:
-            bark['attributes'].update(collar='1')
+            bark['attributes'].update(collar=collar_id)
             barks.add(**(bark['attributes']))
         return flask.jsonify(meta={'accepted': True})
